c4fea6438f27e33e8e706accea051840e0433df6
ParikirtJha <parikirtj424@gmail.com>
2020-06-14 11:26:39 +0530

    getting ready to push an example cachematrix file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+##function to store value of inverse if already calculated
+makeCacheMatrix<-function(x=matrix())
+{
+  i<-NULL ##initialising the inverse 
+  set<-function(matrix) {
+    x<<-matrix                    ##setting value of matrix in parent environment
+    #if new value of matrix is set cached value is made NULL
+    i<<-NULL
+  }
+  get<-function()
+  {
+    x                             ##function returning value of matrix 
+  }
+  setInverse<-function(inverse)   ##function to set inverse of matrix 
+  {
+    i<<-inverse                   ##setting value of inverse in parent environment
+  }
+  
+  getInverse<-function()
+  {
+    i                             ##function returning value of inverse matrix
+  }
+  list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+##function to check if cached value present if not calculates inverse and if present returns cached value
+cacheSolve<-function(x, ...)
+{
+  i<-x$getInverse()
+  
+  if(!is.null(i)){
+    message('getting cached value')                  ##return inverse if already cached
+    return(i) 
+  }
+  data<-x$get()
+  i<-solve(data) %*% data           ##calculate inverse using matrix multiplication
+  x$setInverse(i) 
+  i##return matrix    
 }
